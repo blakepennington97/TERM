b23a5d8a64d4982b88273216afea6b82bdadf672
Blake Pennington <blakepennington97@gmail.com>
2021-02-22 18:20:22 -0600

    starting to add Floating Action Button

--- term_app/lib/progress.dart
+++ term_app/lib/progress.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 class Progress extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => ProgressState();
+  // AddInputButton createState() => AddInputButton();
 }
 
 class ProgressState extends State<Progress> {
@@ -394,3 +395,83 @@ class ProgressState extends State<Progress> {
     ];
   }
 }
+
+
+class AddInputButton extends State<Progress>
+    with SingleTickerProviderStateMixin {
+  bool isOpened = false;
+  AnimationController _animationController;
+  Animation<Color> _animateColor;
+  Animation<double> _animateIcon;
+  Curve _curve = Curves.easeOut;
+
+  @override
+  initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500))
+          ..addListener(() {
+            setState(() {});
+          });
+    _animateIcon =
+        Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
+    _animateColor = ColorTween(
+      begin: Colors.blue,
+      end: Colors.red,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        0.00,
+        1.00,
+        curve: _curve,
+      ),
+    ));
+    super.initState();
+  }
+
+  @override
+  dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  animate() {
+    if (!isOpened) {
+      _animationController.forward();
+    } else {
+      _animationController.reverse();
+    }
+    isOpened = !isOpened;
+  }
+
+  Widget toggle() {
+    return FloatingActionButton(
+      backgroundColor: _animateColor.value,
+      onPressed: animate,
+      tooltip: 'Toggle',
+      child: AnimatedIcon(
+        icon: AnimatedIcons.menu_close,
+        progress: _animateIcon,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return toggle();
+  }
+}
+
+class TestColumn extends StatelessWidget {
+      @override
+      Widget build(BuildContext context) {
+        return Container(
+          color: Colors.purple,
+          child: Column(
+            children: <Widget>[
+              ProgressState(),
+              AddInputButton()
+            ],
+          ),
+        );
+      }
+    }
