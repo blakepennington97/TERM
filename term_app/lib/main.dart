33bcb67fe9cc7901a84333fa327350f0e1e50306
Kyatt Spessert <kspessert@gmail.com>
2021-02-22 22:37:06 -0600

    Added sign up page

--- term_app/lib/main.dart
+++ term_app/lib/main.dart
@@ -1,72 +1,138 @@
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+import 'signup.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new SignIn());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  bool _isLoggedIn = false;
-
-  GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
-
-  _login() async {
-    try {
-      await _googleSignIn.signIn();
-      setState(() {
-        _isLoggedIn = true;
-      });
-    } catch (err) {
-      print(err);
-    }
-  }
-
-  _logout() {
-    _googleSignIn.signOut();
-    setState(() {
-      _isLoggedIn = false;
-    });
-  }
-
+class SignIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-            child: _isLoggedIn
-                ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Image.network(
-                        _googleSignIn.currentUser.photoUrl,
-                        height: 50.0,
-                        width: 50.0,
-                      ),
-                      Text(_googleSignIn.currentUser.displayName),
-                      OutlineButton(
-                        child: Text("Logout"),
-                        onPressed: () {
-                          _logout();
-                        },
-                      )
-                    ],
-                  )
-                : Center(
-                    child: OutlineButton(
-                      child: Text("Login with Google"),
-                      onPressed: () {
-                        _login();
-                      },
-                    ),
-                  )),
-      ),
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: <String, WidgetBuilder>{
+        '/signup': (BuildContext context) => new SignupPage()
+      },
+      home: new SignInPage(),
     );
   }
 }
+
+class SignInPage extends StatefulWidget {
+  @override
+  _SignInState createState() => new _SignInState();
+}
+
+class _SignInState extends State<SignInPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        body: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                  padding: EdgeInsets.fromLTRB(65.0, 140.0, 0.0, 0.0),
+                  child: Text('TERM',
+                      style: TextStyle(
+                          fontSize: 100.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red))),
+            ],
+          ),
+        ),
+        Container(
+            padding: EdgeInsets.only(top: 75.0, left: 20.0, right: 20.0),
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'EMAIL',
+                      labelStyle: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red))),
+                ),
+                SizedBox(height: 20.0),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'PASSWORD',
+                      labelStyle: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red))),
+                  obscureText: true,
+                ),
+                SizedBox(height: 35.0),
+                Container(
+                  alignment: Alignment(1.0, 0.0),
+                  padding: EdgeInsets.only(top: 15.0, left: 20.0),
+                  child: InkWell(
+                    child: Text(
+                      'Forgot Password',
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Montserrat',
+                          decoration: TextDecoration.underline),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 40.0),
+                Container(
+                  height: 40.0,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20.0),
+                    shadowColor: Colors.grey,
+                    color: Colors.red,
+                    elevation: 7.0,
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Center(
+                        child: Text(
+                          'LOGIN',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Montserrat'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )),
+        SizedBox(height: 20.0),
+        Container(
+          height: 40.0,
+          child: Material(
+            borderRadius: BorderRadius.circular(20.0),
+            shadowColor: Colors.grey,
+            color: Colors.red,
+            elevation: 7.0,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushNamed('/signup');
+              },
+              child: Center(
+                child: Text(
+                  'CREATE PROFILE',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Montserrat'),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    ));
+  }
+}
+
+// code cited: https://github.com/rajayogan/flutter-minimalloginUI/blob/master/lib/main.dart
