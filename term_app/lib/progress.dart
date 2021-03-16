b09d3ed3b83e36e8916f9879b5c278bad34ae3ab
Blake Pennington <blakepennington97@gmail.com>
2021-03-16 14:57:36 -0500

    added floating action button class

--- term_app/lib/progress.dart
+++ term_app/lib/progress.dart
@@ -1,13 +1,19 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:fab_circular_menu/fab_circular_menu.dart';
 
 class Progress extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => ProgressState();
-  // AddInputButton createState() => AddInputButton();
+  _ProgressState createState() => _ProgressState();
+  //_InputButtonState createState() => _InputButtonState();
 }
 
-class ProgressState extends State<Progress> {
+class InputButton extends StatefulWidget {
+  @override
+  _InputButtonState createState() => _InputButtonState();
+}
+
+class _ProgressState extends State<Progress> {
   bool isShowingMainData;
 
   @override
@@ -41,11 +47,7 @@ class ProgressState extends State<Progress> {
                   height: 37,
                 ),
                 const Text(
-<<<<<<< HEAD
-                  'Weight',
-=======
                   'Weight/Blood Pressure',
->>>>>>> 6825ac3f5c211759898c4e28b1f143d625d5f0a2
                   style: TextStyle(
                     color: Color(0xff827daa),
                     fontSize: 16,
@@ -378,7 +380,6 @@ class ProgressState extends State<Progress> {
   }
 }
 
-
 class AddInputButton extends State<Progress>
     with SingleTickerProviderStateMixin {
   bool isOpened = false;
@@ -443,34 +444,106 @@ class AddInputButton extends State<Progress>
   }
 }
 
-<<<<<<< HEAD
-class TestColumn extends StatelessWidget {
-      @override
-      Widget build(BuildContext context) {
-        return Container(
-          color: Colors.purple,
-          child: Column(
+class _InputButtonState extends State<InputButton> {
+  final GlobalKey<FabCircularMenuState> fabKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    final primaryColor = Theme.of(context).primaryColor;
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          color: const Color(0xFF192A56),
+          child: Center(
+            child: RaisedButton(
+              onPressed: () {
+                // The menu can be handled programatically using a key
+                if (fabKey.currentState.isOpen) {
+                  fabKey.currentState.close();
+                } else {
+                  fabKey.currentState.open();
+                }
+              },
+              color: Colors.white,
+              child: Text('Toggle menu programatically',
+                  style: TextStyle(color: primaryColor)),
+            ),
+          ),
+        ),
+        floatingActionButton: Builder(
+          builder: (context) => FabCircularMenu(
+            key: fabKey,
+            // Cannot be `Alignment.center`
+            alignment: Alignment.bottomRight,
+            ringColor: Colors.white.withAlpha(25),
+            ringDiameter: 500.0,
+            ringWidth: 150.0,
+            fabSize: 64.0,
+            fabElevation: 8.0,
+            fabIconBorder: CircleBorder(),
+            // Also can use specific color based on wether
+            // the menu is open or not:
+            // fabOpenColor: Colors.white
+            // fabCloseColor: Colors.white
+            // These properties take precedence over fabColor
+            fabColor: Colors.white,
+            fabOpenIcon: Icon(Icons.menu, color: primaryColor),
+            fabCloseIcon: Icon(Icons.close, color: primaryColor),
+            fabMargin: const EdgeInsets.all(16.0),
+            animationDuration: const Duration(milliseconds: 800),
+            animationCurve: Curves.easeInOutCirc,
+            onDisplayChange: (isOpen) {
+              _showSnackBar(
+                  context, "The menu is ${isOpen ? "open" : "closed"}");
+            },
             children: <Widget>[
-              ProgressState(),
-              AddInputButton()
+              RawMaterialButton(
+                onPressed: () {
+                  _showSnackBar(context, "You pressed 1");
+                },
+                shape: CircleBorder(),
+                padding: const EdgeInsets.all(24.0),
+                child: Icon(Icons.looks_one, color: Colors.white),
+              ),
+              RawMaterialButton(
+                onPressed: () {
+                  _showSnackBar(context, "You pressed 2");
+                },
+                shape: CircleBorder(),
+                padding: const EdgeInsets.all(24.0),
+                child: Icon(Icons.looks_two, color: Colors.white),
+              ),
+              RawMaterialButton(
+                onPressed: () {
+                  _showSnackBar(context, "You pressed 3");
+                },
+                shape: CircleBorder(),
+                padding: const EdgeInsets.all(24.0),
+                child: Icon(Icons.looks_3, color: Colors.white),
+              ),
+              RawMaterialButton(
+                onPressed: () {
+                  _showSnackBar(context,
+                      "You pressed 4. This one closes the menu on tap");
+                  fabKey.currentState.close();
+                },
+                shape: CircleBorder(),
+                padding: const EdgeInsets.all(24.0),
+                child: Icon(Icons.looks_4, color: Colors.white),
+              )
             ],
           ),
-        );
-      }
-    }
-=======
-// class TestColumn extends StatelessWidget {
-//       @override
-//       Widget build(BuildContext context) {
-//         return Container(
-//           color: Colors.purple,
-//           child: Column(
-//             children: <Widget>[
-//               ProgressState(),
-//               AddInputButton()
-//             ],
-//           ),
-//         );
-//       }
-//     }
->>>>>>> 6825ac3f5c211759898c4e28b1f143d625d5f0a2
+        ),
+      ),
+    );
+  }
+
+  void _showSnackBar(BuildContext context, String message) {
+    Scaffold.of(context).showSnackBar(SnackBar(
+      content: Text(message),
+      duration: const Duration(milliseconds: 1000),
+    ));
+  }
+}
