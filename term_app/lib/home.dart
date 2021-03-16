caa431da2941a0faa8a5e7be895fdcc24f7499bb
wstosh <wstosh@crimson.ua.edu>
2021-03-16 15:25:01 -0500

    various updates

---
+++ term_app/lib/home.dart
@@ -0,0 +1,59 @@
+import 'progress.dart';
+import 'email.dart';
+import 'profile.dart';
+import 'package:flutter/material.dart';
+
+/// This is the stateful widget that the main application instantiates.
+class MyStatefulWidget extends StatefulWidget {
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static List<Widget> _children = [
+    Progress(),
+    Email(Colors.white),
+    Profile(Colors.black)
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TERM'),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: _children[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.deepPurple,
+        unselectedItemColor: Colors.white54,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.analytics_outlined),
+            label: 'Progess',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.messenger),
+            label: 'Email',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
