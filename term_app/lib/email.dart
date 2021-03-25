f56bda34f77eec9802c74e8b7ca3118b88eb3e69
azubic <azubic@crimson.ua.edu>
2021-03-25 02:09:13 -0500

    added email function

--- term_app/lib/email.dart
+++ term_app/lib/email.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Email extends StatelessWidget {
- final Color color;
+  final Color color;
 
- Email(this.color);
+  Email(this.color);
 
- @override
- Widget build(BuildContext context) {
-   return Container(
-     color: color,
-   );
- }
+  get child => null;
+
+  void _launchEmail(String emailId) async {
+    var url = "mailto:$emailId?subject = Hello World Email";
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'could not send e-mail';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: SingleChildScrollView(
+            child: Center(
+          child: Column(children: <Widget>[
+            ElevatedButton(
+                child: Text('Send Email'),
+                onPressed: () {
+                  _launchEmail('sample@gmail.com');
+                }),
+            SizedBox(
+              height: 20.0,
+            ),
+          ]),
+        )));
+  }
 }
